bc57445f91271f17566463e72c2c61ea65adfcde
Louis Popi <theh2o64@gmail.com>
2017-04-08 08:20:28 +0200

    camera: Use per-chipset path format
    Change-Id: Ia42ddbcdf2e60f87b4d23580510d257eeaacfda5

--- QCamera/HAL2/core/Android.mk
+++ QCamera/HAL2/core/Android.mk
@@ -54,7 +54,7 @@ LOCAL_C_INCLUDES := \
         $(TARGET_OUT_INTERMEDIATES)/include/mm-camera-interface_badger \
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/display \
-        hardware/qcom/media/libstagefrighthw \
+        hardware/qcom/media/$(QCOM_HARDWARE_VARIANT)/libstagefrighthw \
 	system/media/camera/include
 
 # if debug service layer and up , use stub camera!
